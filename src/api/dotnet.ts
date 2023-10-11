c1eec71a6186fc6252bcd3601b1ef01a14002525
Santi Huanacu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-10-11 11:59:38 -0400

    Change file to deploy

--- src/api/dotnet.ts
+++ src/api/dotnet.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5296/api",
+  baseURL: "https://todo-mongo-api.azurewebsites.net/api",
 });
 
 export default api;
